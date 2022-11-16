49d50b672a0e65d0856d1a43c5735289fab66f9a
cfrankovich <carson@frankovich.dev>
2022-11-16 09:36:33 -0500

    Pretty printing

--- Debug.sh
+++ Debug.sh
@@ -1,37 +1,62 @@
 # TODO: implement debugging features
 
+# Colors :D #
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+RESET='\033[0m' 
+BOLD=$(tput bold)
+NORM=$(tput sgr0)
+
+PrintNegative () {
+    echo -e "${BOLD}[${RED}-${RESET}${BOLD}]${NORM} $1"
+}
+
+PrintPositive () {
+    echo -e "${BOLD}[${GREEN}+${RESET}${BOLD}]${NORM} $1"
+}
+
+PrintNeutral () {
+    echo -e "${BOLD}[${BLUE}o${RESET}${BOLD}]${NORM} $1"
+}
+
+Run () {
+    PrintNeutral "Running program...\n"
+    java -cp ./build dev/frankovich/sim/Simulation $1
+    echo -ne "\n"
+    PrintNeutral "Program exited with exit code $?"
+}
+
+Clean () {
+    PrintNeutral "Cleaning build directory..."
+    rm -rf ./build/*
+    PrintPositive "Cleaned!"
+}
+
+Compile () {
+    PrintNeutral "Compiling..."
+    javac -d ./build -sourcepath ./src -cp ./build ./src/dev/frankovich/sim/Simulation.java
+    if [ $? != 0 ]; then
+        PrintNegative "Failed compiling!"
+        exit 0
+    else
+        PrintPositive "Compiled!"
+    fi
+}
+
 # Cleans out all the class files in the build directory
 if [ "$1" == "clean" ]; then
-    echo "[o] Cleaning build directory..."
-    rm -rf ./build/*
-    echo "[+] Cleaned!"
+    Clean
     exit 0
 fi
 
 # Just compiles and exits
 if [ "$1" == "compile" ]; then
-    echo "[o] Compiling..."
-    javac -d ./build -sourcepath ./src -cp ./build ./src/dev/frankovich/sim/Simulation.java
-    if [ $? != 0 ]; then
-        echo "[-] Failed compiling!"
-        exit 0
-    else 
-        echo "[+] Finished compiling!" 
-    fi
+    Compile
     exit 0
 fi
 
 # Compiles and runs program with args given
-echo "[o] Cleaning build directory..."
-rm -rf ./build/*
-echo "[+] Cleaned!"
-echo "[o] Compiling..."
-javac -d ./build -sourcepath ./src -cp ./build ./src/dev/frankovich/sim/Simulation.java
-if [ $? != 0 ]; then
-        echo "[-] Failed compiling!"
-        exit 0
-else
-    echo "[+] Finished compiling!" 
-fi
-java -cp ./build dev/frankovich/sim/Simulation $@
-echo "[o] Program exited with exit code $?"
+Clean
+Compile
+Run $@
